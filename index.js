dca71d7aad8d9e225d9fd40072ce4c2d50845428
AleSolis24 <aletitan24@icloud.com>
2024-01-13 16:47:52 -0500

    update

--- index.js
+++ index.js
@@ -1,58 +1,95 @@
 const inquirer = require("inquirer");
 const { Square, Triangle, Circle } = require('./library/shape');
+const { writeFile } = require("fs/promises");
 
 class SVGLOGO {
     constructor() {
-        this.color = "";
-        this.shape = "";
         this.text = "";
+        this.textColor = "";
         this.shapeLayout = null;
+        this.shapeColor = "";
     }
-
+    //this is seeting up the prompt to allow the user to answer the questions when using node 
     async creatingShape() {
         const userInput = await inquirer.prompt([
             {
                 type: 'input',
-                name: 'color',
-                message: 'Enter color:',
+                name: 'text',
+                message: 'Enter text',
+                validate: (input) => input.length <= 3,
+            },
+            {
+                type: 'input',
+                name: 'textColor',
+                message: 'Enter text color',
             },
             {
                 type: 'list',
                 name: 'shape',
-                message: 'Pick a shape',
+                message: 'Choose a shape',
                 choices: ['Circle', 'Triangle', 'Square'],
             },
             {
                 type: 'input',
                 name: 'shapeColor',
-                message: 'Choose shape color:',
+                message: 'Pick a shape color',
             },
         ]);
 
-        this.color = userInput.color;
-        this.shape = userInput.shape;
-
-        switch (this.shape) {
-            case 'Square':
-                this.shapeLayout = new Square();
-                break;
+        const { text, textColor, shape, shapeColor } = userInput;
+    
+        let shapeLayout;
+        switch (shape) {
             case 'Circle':
-                this.shapeLayout = new Circle();
+                shapeLayout = new Circle();
                 break;
-            case 'Triangle':
-                this.shapeLayout = new Triangle();
+            case 'Square':
+                shapeLayout = new Square();
+                break;
+            default:
+                shapeLayout = new Triangle();
                 break;
         }
+        shapeLayout.setColor(shapeColor);
 
-        this.shapeLayout.setColor(userInput.shapeColor);
+        this.text = text;
+        this.textColor = textColor;
+        this.shapeLayout = shapeLayout;
+        this.shapeColor = shapeColor;
+
+        // this is allowing the logo.svg file to be created 
+        const shapeContent = this.shapeContent();
+        await writeFile("logo.svg", shapeContent);
+        console.log("Created logo.SVG!!!!!")
+        
+    }
+    // this is formating the shape and text using get color from shape.js file and adding the text, also used else if statement to change between shapes
+    shapeContent() {
+        if (this.shapeLayout instanceof Triangle) {
+            return `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+                <polygon points="0,200 300,200 150,0" fill="${this.shapeLayout.getColor()}" />
+                <text x="150" y="100" text-anchor="middle" fill="${this.textColor}" font-size="45">${this.text}</text>
+              </svg>`;
+        } else if (this.shapeLayout instanceof Circle) {
+            return `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="150" cy="100" r="50" fill="${this.shapeLayout.getColor()}" />
+                <text x="150" y="100" text-anchor="middle" fill="${this.textColor}" font-size="45">${this.text}</text>
+              </svg>`;
+        } else if (this.shapeLayout instanceof Square) {
+            return `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+                <rect x="0" y="0" width="300" height="200" fill="${this.shapeLayout.getColor()}" />
+                <text x="150" y="100" text-anchor="middle" fill="${this.textColor}"font-size="45">${this.text}</text>
+              </svg>`;
+        } else {
+            console.error('Invalid shape type.');
+            return null;
+        }
     }
 }
-
-
+// this is the function that will allow the SVG to be created 
 async function createSVGLogo() {
     const logo = new SVGLOGO();
     await logo.creatingShape();
-    console.log(logo); 
 }
-
+// this is calling the creatSVGLogo function 
 createSVGLogo();
--- library/shape.js
+++ library/shape.js
@@ -1,27 +1,51 @@
 class Shape {
     constructor(){
         this.color=''
+        this.text =''
     }
     setColor(color){
         this.color=(color);
     }
+    setText(text){
+        this.text = (text)
+    }
 }
 
 class Triangle extends Shape {
-    render(){
-        return'polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="${this.color}'
+    getType() {
+       return'polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="${this.color}'
+    }
+
+    getColor() {
+        return this.color
+    }
+
+    getText() {
+        return this.text
     }
 };
 
 class Square extends Shape {
-    render(){
+    getType() {
         return '<rect x="50" height="200" width="200" fill="${this.color}'
     }
+    getColor() {
+        return this.color
+    }
+
 };
 
 class Circle extends Shape{
     render(){
         return'<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="${this.color}'
+    }
+
+    getColor () {
+        return this.color
+    }
+
+    getText () {
+        return this.text
     }
 };
 
